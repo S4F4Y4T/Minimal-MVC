a0ad4a018f2169c0468ebc345b86061e1289b99c
S4F4Y4T <safayatmahmud6969@gmail.com>
2021-03-26 10:33:43 +0600

    removed extra controller methods

--- app/controller/Indexs.php
+++ app/controller/Indexs.php
@@ -10,42 +10,10 @@
 		}
 		
 		
-		public function header(){
-		 $sid = session_id();
-	    $cart = array(
-		'table'			 => array('table' => 'cart'),
-		//'pkey'			 => array('pkey' => 'cartid'),
-		'selectcond' => array('count' => 'COUNT(*)'),
-		//'orderby'	   => array('order' => 'DESC'),
-		//'limit' 	 => array('start' => 2,'limit' => 3),
-		'wherecond'=> array('where' =>array('sid' => $sid))
-	);
 	
-	    $Model = $this->load->model("Madmin");
-	    $count = $Model->fetchdata($cart);
-	    if($count[0][0] > 0){
-	        $data['cart'] = Session::get("total");
-	    }else{
-	        $data['cart'] = "empty";
-	    }
-	
-	    $this->load->view("inc/header",$data);
-	}
-	
-	public function error(){
-	    $this->header();
-	    echo '<h1 style="margin: 25%;font-size: 51px;font-weight: bold;color: red;">404 Page Not Found</h1>';
-	    $this->load->view("inc/footer");
-	}
-	
-	public function about(){
-	    $this->header();
-	    $this->load->view("aboutus");
-	    $this->load->view("inc/footer");
-	}
 
 	public function home(){
-		    $this->header();
+		
 		    
 		$pcount = array(
           'table'			 => array('table' => 'product'),
@@ -142,12 +110,12 @@
                     		//'wherecond'=> array('where' =>array('type' => 2))
                     	);
 	
-	$data["newpro"] = $Model->fetchdata($data);
-	$data["topbrand"] = $Model->join($top);
-	$data["feature"] = $Model->fetchdata($feature);
-	$data["slide"] = $Model->fetchdata($brand);
-	$data["pcount"] = $Model->fetchdata($pcount);
-	$data["fcount"] = $Model->fetchdata($fcount);
+			$data["newpro"] = $Model->fetchdata($data);
+			$data["topbrand"] = $Model->join($top);
+			$data["feature"] = $Model->fetchdata($feature);
+			$data["slide"] = $Model->fetchdata($brand);
+			$data["pcount"] = $Model->fetchdata($pcount);
+			$data["fcount"] = $Model->fetchdata($fcount);
 	
 	
 	        $this->load->view("inc/slide",$data);
@@ -156,82 +124,7 @@
 		}
 		
 		
-		public function details($id = NULL){
-		    $data = array(
-		'TableMain'		 => array('TableMain' => 'product'),
-		'Tablea'			   => array('Tablea'    => 'category'),
-		'Tableb'			 => array('Tableb'    => 'brand'),
-		'Tablec'			 => array('Tablec'    => 'subcat'),
-
-		'Mainconda'			   => array('Mainconda' => 'category'),
-		'Maincondb'			 => array('Maincondb' => 'brand'),
-		'Maincondc'			 => array('Maincondc' => 'subcategory'),
-
-		'Conda'			   => array('Conda' => 'catid'),
-		'Condb'			 => array('Condb' => 'brandid'),
-		'Condc'			 => array('Condc' => 'subid'),
-
-		'selectcond' => array('select' => '*'),
-		//'orderby'	  => array('order'  => 'DESC'),
-		'pkey'       => array('pkey'   => 'id'),
-		//'limit' 	  => array('start'  => 0,'limit' => 2),
-		'wherecond'=> array('where'  =>array('id' => base64_decode($id)))
-	);
-	
-	$category = array(
-		'table'			 => array('table' => 'category'),
-		'pkey'			 => array('pkey' => 'catid'),
-		'selectcond' => array('select' => '*'),
-		'orderby'	   => array('order' => 'DESC'),
-		//'limit' 	 => array('start' => 2,'limit' => 3),
-		//'wherecond'=> array('where' =>array('catid' => 156))
-	);
-
-
-	 $Model   	  	   = $this->load->model("Madmin");
-	 $data["details"]  = $Model->join($data);
-	 $data["category"] = $Model->fetchdata($category);
-		    $this->header();
-		    $this->load->view("preview",$data);
-		    $this->load->view("inc/footer");
-		}
 		
-	public function search(){
-	    if(!($_POST)){
-			header("Location:".BASE);
-		}else{
-		$input = $this->load->valid("Validation");
-
-		$input->post("search")
-					->schar()
-					->isempty()
-					->post();
-
-		if($input->submit()){
-
-			$q     = $input->value["search"];
-
-			$data = array(
-        		'table'			 => array('table' => 'product'),
-        		//'pkey'			 => array('pkey' => 'catid'),
-        		'selectcond' => array('select' => '*'),
-        		//'orderby'	   => array('order' => 'DESC'),
-        		//'limit' 	 => array('start' => 2,'limit' => 3),
-        		'search'    => array('name' => $q),
-        		'wherecond'=> array('where' =>array('name' => $q,'productDetails' => $q))
-        	);
-        	
-			$Model = $this->load->model('Madmin');
-			$data['product'] = $Model->fetchdata($data);
-
-			$this->header();
-			$this->load->view("products",$data);
-			$this->load->view("inc/footer");
-		}else{
-			   header("Location:".BASE);
-		}
-	  }
-	}
 		
 }
 ?>
